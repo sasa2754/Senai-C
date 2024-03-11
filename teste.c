1a5c3016cc75fe172aee62d9a06b76d42cf6a4ec
sasa2754 <160642163+sasa2754@users.noreply.github.com>
2024-03-11 17:08:59 -0300

    SomaDoisNum

---
+++ teste.c
@@ -0,0 +1,16 @@
+//Oi
+#include <stdio.h>
+
+int main(){
+    int num1, num2, total;
+    printf("Digite o primeiro número: ");
+    scanf("%i",&num1);
+    printf("Digite o segundo número: ");
+    scanf("%i",&num2);
+    
+    total = num1 + num2;
+    printf("O total é: %i", total);
+
+
+    return 0;
+}
