9dec30b3d821e107b6b7cd3b2c578733d36c773f
sasa2754 <160642163+sasa2754@users.noreply.github.com>
2024-03-12 14:13:26 -0300

    Fahrenheit.c

--- Fahrenheit.c
+++ Fahrenheit.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    float celcius, fah;
+    float celcius, fah, kelvin;
     celcius = 25.5;
 
     fah = (celcius*9/5)+32;
+    kelvin = celcius + 273.15;
 
     printf("Temperatura em celcius: %f\n", celcius);
-    printf("Temperatura em Fahrenheit: %f", fah);
+    printf("Temperatura em Fahrenheit: %f\n", fah);
+    printf("Temperatura em Kelvin: %f", kelvin);
+
     return 0;
 }
