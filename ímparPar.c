e46c70206204db8d32aa40ce8b063577b4344d5c
sasa2754 <160642163+sasa2754@users.noreply.github.com>
2024-03-12 14:19:06 -0300

    ÍmparPar

---
+++ ímparPar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num = 5;
+
+    if (num%2==0){
+        printf("The number is even!");
+    }
+    else{
+        printf("The number is odd!");
+    }
+
+    return 0;
+}
